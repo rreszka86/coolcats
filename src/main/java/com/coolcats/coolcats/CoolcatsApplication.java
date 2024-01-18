d1d00f1e93a88c819bc8462cd4ae92be0fa23809
Michał Reszka <michal.reszka86@gmail.com>
2024-01-18 23:02:29 +0100

    Initial commit

---
+++ src/main/java/com/coolcats/coolcats/CoolcatsApplication.java
@@ -0,0 +1,13 @@
+package com.coolcats.coolcats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolcatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolcatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coolcats/coolcats/CoolcatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolcats.coolcats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolcatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
